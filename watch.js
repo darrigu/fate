e2b220d3633f3a7668a38faff44d7f75e628e478
LeBakaSable <hugogole.ito@gmail.com>
2025-02-27 02:17:29 +0100

    feat: merge devServer.js, watch.js & cold reload some files

--- watch.js
+++ watch.js
@@ -1,4 +1,7 @@
 const { spawn } = require('child_process');
+const { WebSocketServer } = require('ws');
+const { watchFile } = require('fs');
+const path = require('path');
 
 const cmd = (program, ...args) => {
    console.log('CMD:', program, ...args);
@@ -14,4 +17,34 @@ const cmd = (program, ...args) => {
 
 cmd('tsc', '-w');
 cmd('http-server', '-p', '8080', '-a', '127.0.0.1', '-s', '-c-1');
-cmd('node', './devServer.js');
+
+const wss = new WebSocketServer({
+   port: 5000,
+});
+
+/** @type {import('ws').WebSocket[]} */
+const websockets = [];
+
+wss.on('connection', (ws) => {
+   websockets.push(ws);
+
+   ws.on('close', () => {
+      websockets.splice(websockets.indexOf(ws), 1);
+   });
+});
+
+const COLD_RELOAD_FILES = ['index.html', 'main.js'];
+
+COLD_RELOAD_FILES.forEach(file => {
+   watchFile(path.join(__dirname, file), { interval: 50 }, () => {
+      websockets.forEach(socket => socket.send('cold'));
+   });
+});
+
+const HOT_RELOAD_FILES = ['game.js'];
+
+HOT_RELOAD_FILES.forEach(file => {
+   watchFile(path.join(__dirname, file), { interval: 50 }, () => {
+      websockets.forEach(socket => socket.send('hot'));
+   });
+});
