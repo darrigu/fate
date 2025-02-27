f6a4d40ffae1a667e3349b30aecc09b405056262
LeBakaSable <hugogole.ito@gmail.com>
2025-02-27 01:46:14 +0100

    feat: handle window resize

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const SCREEN_HEIGHT = 9 * SCREEN_FACTOR;
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 const ctx = canvas.getContext('2d');
 ctx.imageSmoothingEnabled = false;
 const backImageData = new ImageData(SCREEN_WIDTH, SCREEN_HEIGHT);
--- main.ts
+++ main.ts
@@ -7,6 +7,12 @@ const SCREEN_HEIGHT = 9*SCREEN_FACTOR;
 const canvas = document.querySelector('canvas')!;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+});
+
 const ctx = canvas.getContext('2d')!;
 ctx.imageSmoothingEnabled = false;
 
